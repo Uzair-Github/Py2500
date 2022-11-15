7c3f98143170dfd1e894e49dac7e67c61f565185
M. Uzair <29971892+Uzair-Github@users.noreply.github.com>
2022-11-15 23:24:18 +0500

    Initial commit

---
+++ Mod-01/type.py
@@ -0,0 +1,13 @@
+#Checking Type
+Number = 55
+print(Number)
+
+print(Number,type(Number))
+
+Number = "55"
+print(Number,type(Number))
+
+#Converting Type
+Number = int(Number)
+
+print(Number,type(Number))
---
+++ Mod-02/basic-operators.py
@@ -0,0 +1,27 @@
+a = 5
+b = 2
+c = 3
+
+#addition
+print('sum of a & b is = ', a+b)
+
+#check quotations print text instead of number
+print('a+b')
+
+#subtraction
+print('subtraction of a & b is = ',a-b)
+print('subtraction of c & b is = ',c-b)
+
+#multiplicaion
+print('multiplication of a & b is = ',a*b)
+
+#division
+print('division of a divided by b is = ',a/b)
+
+#remainder
+print('remainder of a divided by b is = ',a%b)
+
+#power (method 1)
+print(a**b)
+#power (method 2)
+print(pow(a,c))
