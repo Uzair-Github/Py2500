9d2663ad6ce8744bc753dd4bf37b4e54cc77b9b6
Uzair-Github <siegine.893@gmail.com>
2022-11-18 21:25:12 +0500

    Initial commit

---
+++ Mod-02/String.py
@@ -0,0 +1,10 @@
+#using string
+name = input("Enter your Name: ")
+print (name)
+
+#Using with sentence i.e. string
+age = input("Enter your age: ")
+print ("Your age is " , age)
+
+#combining variables to make 1 string
+print ("Your name is " , name , "and your age is ", age)
