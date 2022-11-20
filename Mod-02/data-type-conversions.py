d8e8d024379feeab6475edb86b5aa7b8cafcb64e
Uzair-Github <siegine.893@gmail.com>
2022-11-20 14:15:01 +0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	20-Nov-2022/while.py

--- Mod-02/data-type-conversions.py
+++ Mod-02/data-type-conversions.py
@@ -1,47 +1,47 @@
 a = 7
 
 b = bin(a)
-print (b)
+print(b)
 
 c = 25
 
 d = bin(c)
-print (d)
+print(d)
 
-#conversion shown with sentence form
+# conversion shown with sentence form
 
-print ('We have',a ,'in binary form ', b)
-print ('We have',c ,'in binary form ', d)
+print('We have', a, 'in binary form ', b)
+print('We have', c, 'in binary form ', d)
 
-#conversion to decimal from binary
+# conversion to decimal from binary
 
 e = b
-print ('e is equal' , e)
-f = ('We have', e , 'converted to decimal' , int (e,2))
-print (f)
+print('e is equal', e)
+f = ('We have', e, 'converted to decimal', int(e, 2))
+print(f)
 
-#conversion to octal number
+# conversion to octal number
 g = 10
-print ('g is equal' , g)
+print('g is equal', g)
 h = oct(g)
-print (h)
+print(h)
 
-#conversion to decimal from octal
+# conversion to decimal from octal
 
 i = h
-print ('i is equal' , i)
-#when reconversion, int (variable,data converted into binary/octal/hexadecimal)
-j = ('We have', i , 'converted to decimal' , int (i,8))
-print (j)
+print('i is equal', i)
+# when reconversion, int (variable,data converted into binary/octal/hexadecimal)
+j = ('We have', i, 'converted to decimal', int(i, 8))
+print(j)
 
-#conversion to hexa-decimal number
+# conversion to hexa-decimal number
 k = 13
-print ('k is equal' , k)
+print('k is equal', k)
 l = hex(k)
-print (l)
+print(l)
 
-#conversion to decimal from hexa-decimal
+# conversion to decimal from hexa-decimal
 m = l
-print ('m is equal' , m)
-n = ('We have', m , 'converted to decimal' , int (m,16))
-print (n)
+print('m is equal', m)
+n = ('We have', m, 'converted to decimal', int(m, 16))
+print(n)
