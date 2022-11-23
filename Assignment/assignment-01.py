757773b6e991c19e2a8d39147991e014e70b4aed
Uzair-Github <siegine.893@gmail.com>
2022-11-23 20:49:43 +0500

    Merge remote-tracking branch 'origin/main'

---
+++ Assignment/assignment-01.py
@@ -0,0 +1,9 @@
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+num3 = input('Enter third number: ')
+
+# Add three numbers
+sum = float(num1) + float(num2) + float(num3)
+
+# Display the sum
+print('The sum of {0},{1} and {2} is {3}'.format(num1, num2, num3, sum))
---
+++ mod-1-quiz.py
@@ -0,0 +1,5 @@
+x = 1 | 0
+print ("1 or 0 is:",x)
+
+x = 1 & 0
+print ("1 and 0 is:",x)
